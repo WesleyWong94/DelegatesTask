0763765948682dbe8ffca66d3b144faa63b0bee5
unknown <103055362@student.swin.edu.au>
2020-05-02 20:57:09 +1000

    Upload First Commit

---
+++ Delegate_Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FileParser;
+
+public delegate List<List<string>> Parser(List<List<string>> data);
+
+namespace Delegate_Exercise
+{
+    class Delegate_Exercise
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C:\\Users\\wesle\\Desktop\\Delegate_Task\\Dip_Delegates_Task\\DataFiles\\data.csv");
+        }
+
+        public static List<List<string>> RemoveHashes(List<List<string>> data) {
+            foreach(var row in data) {
+                for (var index = 0; index < row.Count; index++) {
+                    if(row[index][0] == '#')
+                        row[index] = row[index].Remove(0,1);
+ 
+                }
+            }
+            return data;
+            
+        }
+    }
+}
