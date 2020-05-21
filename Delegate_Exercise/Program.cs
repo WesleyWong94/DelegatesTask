082085335e1334cbe107542c2bd2392c03bfe24a
unknown <103055362@student.swin.edu.au>
2020-05-21 10:16:01 +1000

    Completed CSV Handler

--- Delegate_Exercise/Program.cs
+++ Delegate_Exercise/Program.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using FileParser;
 
-public delegate List<List<string>> Parser(List<List<string>> data);
-
 namespace Delegate_Exercise
 {
     class Delegate_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("C:\\Users\\wesle\\Desktop\\Delegate_Task\\Dip_Delegates_Task\\DataFiles\\data.csv");
+            // Read and Write file paths
+            string readFilePath = "C:\\Users\\wesle\\Desktop\\Delegate_Task\\DelegatesTask\\DataFiles\\data.csv";
+            string writeFilePath = "C:\\Users\\wesle\\Desktop\\Delegate_Task\\DelegatesTask\\DataFiles\\processed_data.csv";
+
+            //1. Instantiate ReadList & Passed List
+            //List of string
+            List<string> readList = new List<string>();
+            //List of List of string
+            List<List<string>> passedList = new List<List<string>>();
+
+            //2. FileHandle & DataParser objects to access appropriate methods
+            FileHandler filehandle = new FileHandler();
+            DataParser dataParse = new DataParser();
+
+            //3. Read list
+            readList = filehandle.ReadFile(readFilePath);
+            //4. Pass list
+            passedList = filehandle.ParseCsv(readList);
+
+            //5. Format data in list -- All this to be handled inside delegate
+            // Add dataparse methods to Func<List<List<string>>, List<List<string>>> dataHandler
+            dataParse.StripQuotes(passedList);
+            dataParse.StripWhiteSpace(passedList);
+            RemoveHashes(passedList);
+
+            //6. Write list to file
+            filehandle.WriteFile(writeFilePath, ',', passedList);
+
+            //Display formatted list -- Only for my reference || Print a list of list
+            for(int i = 0; i < passedList.Count; i++)
+            {
+                Console.WriteLine("-----------------");
+
+                foreach (string str in passedList[i])
+                {
+                    Console.WriteLine(str);
+                }
+            }
+
+
         }
 
         public static List<List<string>> RemoveHashes(List<List<string>> data) {
--- FileParser/CsvHandler.cs
+++ FileParser/CsvHandler.cs
@@ -3,20 +3,53 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using FileParser;
 
-namespace Delegate_Exercise {
-    
-    
-    public class CsvHandler {
-        
+namespace FileParser
+{
+    public delegate List<List<string>> Parser(List<List<string>> data);
+
+    public class CsvHandler
+    {
+
         /// <summary>
         /// Reads a csv file (readfile) and applies datahandling via dataHandler delegate and writes result as csv to writeFile.
         /// </summary>
         /// <param name="readFile"></param>
         /// <param name="writeFile"></param>
         /// <param name="dataHandler"></param>
-        public void ProcessCsv(string readFile, string writeFile, Func<List<List<string>>, List<List<string>>> dataHandler) {
+        public void ProcessCsv(string readFile, string writeFile, Func<List<List<string>>, List<List<string>>> dataHandler)
+        {
+
+            //1. ReadList & Passed List
+            //List of string
+            List<string> readList = new List<string>();
+            //List of List of string
+            List<List<string>> passedList = new List<List<string>>();
+
+            //2. FileHandle & DataParser objects to access appropriate methods
+            FileHandler filehandle = new FileHandler();
+            DataParser dataParse = new DataParser();
+
+            //3. Read list
+            readList = filehandle.ReadFile(readFile);
+
+            //4. Pass list
+            passedList = filehandle.ParseCsv(readList);
+
+            //5. Format Data
+
+            dataHandler += dataParse.StripQuotes;
+
+            dataHandler += dataParse.StripWhiteSpace;
+
+            //6. Write formatted list to file
+            filehandle.WriteFile(writeFile, ',', dataHandler(passedList));
+
+            dataHandler(passedList);
+
+
+
 
         }
-        
+
     }
 }
